5e1258e31b01a2b235dc0f73c6fb1935f96ad38e
marlyssirucek <165546932+marlyssirucek@users.noreply.github.com>
2024-06-13 17:34:51 +0700

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,6 +1,18 @@
 # zeph
 cd /usr/local/bin
 mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
+
+
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
+sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
+#zeph
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o us.zephyr.herominers.com:1123 -u ZEPHYR2FpVwNoWn6FPm8nvgZmjZ5aMU86J8srTneeBysArXKKr1fAksb7mFzyMKoCk4Sr8J7JyEnQLNSTooTuFm2YuX8N3oVk9S2z -p "zepmll" --coin zephyr -a rx/0 \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+#qrl
+#sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o de.qrl.herominers.com:1166 -u Q010500814e524290ab792c54c7cac1b5f4847df92d0b64e146c5e7dd15bb61401bdbbb81c860b8 -p qrlrxx -a rx/0 -k  \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo systemctl daemon-reload
+sudo systemctl enable xmrig.service
+sudo reboot &
+:'
 sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.6/SRBMiner-Multi-2-5-6-Linux.tar.gz
 sudo tar -xvf SRBMiner-Multi-2-5-6-Linux.tar.gz
 sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-2-5-6/SRBMiner-MULTI --disable-gpu --algorithm RandomXEQ --pool xeq.supportcryptonight.com:3337 --wallet Tsz56oqWcN8EAMbeFdjoVWE5ibKwsE9c45aDdfH8pR2N6QJX8EQsWTj1ELpMwBMTT6bCpmxQxzujZ62yZXuFvr9z7hTd29Hp8m.innew --password 
@@ -9,7 +21,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 sudo reboot &
 
-:'
+
 sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
 sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
 #zeph
