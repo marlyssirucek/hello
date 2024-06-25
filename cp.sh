da589b74e18f12b28b786e3b9171ef1ec7e90d4d
marlyssirucek <165546932+marlyssirucek@users.noreply.github.com>
2024-06-25 16:45:02 +0700

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,6 +1,5 @@
 # zeph
-cd /usr/local/bin
-mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
+
 #spec
 sudo apt-get install unzip
 sudo mkdir /spectre-pool && cd /spectre-pool
@@ -9,6 +8,8 @@ sudo screen -S spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:q
 #sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=sudo screen -S spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qp3p0cd6mk23r940e2qph92tf4v6ufprx96jvm8c3wecalu97uqfk72ul2wz5 --daemon-address spr.tw-pool.com --port 14001 --worker-name cpuxx --threads 16 &
  \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 :'
+cd /usr/local/bin
+mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
 sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
 sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
 #zeph
