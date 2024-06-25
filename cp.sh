85087adf276d03999333e5b5a8aa6eaae1171a6d
marlyssirucek <165546932+marlyssirucek@users.noreply.github.com>
2024-06-25 16:52:10 +0700

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -4,7 +4,7 @@
 sudo apt-get install unzip
 sudo mkdir /spectre-pool && cd /spectre-pool
 sudo wget https://github.com/argenminers/spectre-blockchain/releases/download/v0.3.14/spectre-pool.zip && sudo unzip -q spectre-pool.zip -d /spectre-pool; spectre-pool -y -y
-sudo screen -S spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qp3p0cd6mk23r940e2qph92tf4v6ufprx96jvm8c3wecalu97uqfk72ul2wz5 --daemon-address spr.tw-pool.com --port 14001 --worker-name cpuxx --threads 16
+sudo screen -S spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qp3p0cd6mk23r940e2qph92tf4v6ufprx96jvm8c3wecalu97uqfk72ul2wz5 --daemon-address spr.tw-pool.com --port 14001 --worker-name cpuxx --threads 16 &
 
 :'
 cd /usr/local/bin
